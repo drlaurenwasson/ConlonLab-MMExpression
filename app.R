341d54e9506c9f48e42ece7060e13e4f8062720e
Lauren Wasson <dr.lauren.wasson@gmail.com>
2024-04-02 12:28:03 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -28,14 +28,14 @@ ui <- fluidPage(
         ),
         checkboxGroupInput(inputId = "timepoint",
                          label = "Time Points", 
-                         choices = c("E09", "E10", "E11", "E12", "E13", "E14", "E15", "E16"),
-                         selected = c("E09", "E10", "E11", "E12", "E13", "E14", "E15", "E16"),
+                         choices = c("E09.5", "E10.5", "E11.5", "E12.5", "E13.5", "E14.5", "E15.5", "E16.5"),
+                         selected = c("E09.5", "E10.5", "E11.5", "E12.5", "E13.5", "E14.5", "E15.5", "E16.5"),
                          inline = FALSE,
                          width = NULL,
                          choiceNames = NULL,
                          choiceValues = NULL
         ),
-        selectInput("normalize", "Select Time Point to Normalize to", choices = c("E09", "E10", "E11", "E12", "E13", "E14", "E15", "E16")),
+        selectInput("normalize", "Select Time Point to Normalize to", choices = c("E09.5", "E10.5", "E11.5", "E12.5", "E13.5", "E14.5", "E15.5", "E16.5")),
         actionButton(inputId = "update", label = "update"),
         plotOutput(outputId = "scatterplot"),
         downloadButton("DownloadPlot", "Download Plot")
@@ -48,14 +48,14 @@ ui <- fluidPage(
         ),
         checkboxGroupInput(inputId = "rnatimepoint",
                            label = "Time Points", 
-                           choices = c("E10.5", "E11.5", "E12.5", "E13.5", "E14.5", "E15.5", "E16.5"),
-                           selected = c("E10.5", "E11.5", "E12.5", "E13.5", "E14.5", "E15.5", "E16.5"),
+                           choices = c("E09.5", "E10.5", "E12.5", "E14.5", "E16.5"),
+                           selected = c("E09.5", "E10.5", "E12.5", "E14.5", "E16.5"),
                            inline = FALSE,
                            width = NULL,
                            choiceNames = NULL,
                            choiceValues = NULL
         ),
-        selectInput("normalize2", "Select Time Point to Normalize to", choices = c("E09.5")),
+        selectInput("normalize2", "Select Time Point to Normalize to", choices = c("E09.5", "E10.5", "E12.5", "E14.5", "E16.5")),
         actionButton(inputId = "rnaupdate", label = "update"),
         plotOutput(outputId = "rnascatterplot"),
         downloadButton("rnaDownloadPlot", "Download Plot")
@@ -68,8 +68,8 @@ ui <- fluidPage(
         ),
         checkboxGroupInput(inputId = "rnaproteintimepointr",
                            label = "RNA Time Points", 
-                           choices = c("E10.5", "E11.5", "E12.5", "E13.5", "E14.5", "E15.5", "E16.5"),
-                           selected = c("E10.5", "E11.5", "E12.5", "E13.5", "E14.5", "E15.5", "E16.5"),
+                           choices = c("E09.5", "E10.5", "E12.5", "E14.5", "E16.5"),
+                           selected = c("E09.5", "E10.5", "E12.5", "E14.5", "E16.5"),
                            inline = FALSE,
                            width = NULL,
                            choiceNames = NULL,
@@ -77,14 +77,14 @@ ui <- fluidPage(
         ),
         checkboxGroupInput(inputId = "rnaproteintimepointp",
                            label = "Protein Time Points", 
-                           choices = c("E09", "E10", "E11", "E12", "E13", "E14", "E15", "E16"),
-                           selected = c("E09", "E10", "E11", "E12", "E13", "E14", "E15", "E16"),
+                           choices = c("E09.5", "E10.5", "E11.5", "E12.5", "E13.5", "E14.5", "E15.5", "E16.5"),
+                           selected = c("E09.5", "E10.5", "E11.5", "E12.5", "E13.5", "E14.5", "E15.5", "E16.5"),
                            inline = FALSE,
                            width = NULL,
                            choiceNames = NULL,
                            choiceValues = NULL
         ),
-        selectInput("normalize", "Select Time Point to Normalize to", choices = c("E09")),
+        selectInput("normalize3", "Select Time Point to Normalize to", choices = c("E09.5", "E10.5", "E12.5", "E14.5", "E16.5")),
         actionButton(inputId = "rnaprotupdate", label = "update"),
         plotOutput(outputId = "rnaprotscatter"),
         downloadButton("DownloadPlot3", "Download Plot")
@@ -116,7 +116,7 @@ server <- function(input, output, session) {
   })
   
   df2=NULL
-  df3=NULL
+  df4=NULL
   dfrp=NULL
   
   #When "update" is pressed for proteins:
@@ -135,7 +135,7 @@ server <- function(input, output, session) {
     for (p in {proteinsupdate}){
       #Check to see if protein is in the database
       if (p %in% rownames(rawvalues)){
-        a<- getdata(p,{normalizeupdate})
+        a<- getdata(p,{timepointsupdate},{normalizeupdate})
         df2=rbind(df2,a)
       }
 
@@ -177,12 +177,11 @@ server <- function(input, output, session) {
   rownames(rawvalues)<- make.names(genes, unique = TRUE)
   
   #Get a Protein data frame for the proteins of interest
-  getdata<- function(protein,timepoint){
-    tp<- c("E09", "E10", "E11", "E12", "E13", "E14", "E15", "E16")
-    normtp<- tp[tp %in% timepoint]
+  getdata<- function(protein,timepoint,normtp){
+    tp<- c("E09.5", "E10.5", "E11.5", "E12.5", "E13.5", "E14.5", "E15.5", "E16.5")
     protein<- protein[protein %in% rownames(rawvalues)]
     df<- as.data.frame(t(rawvalues[rownames(rawvalues)==protein,]))
-    df$day<- c(rep("E09",3), rep("E10",3), rep("E11",3), rep("E12",3), rep("E13",3), rep("E14",3), rep("E15",3), rep("E16",3))
+    df$day<- c(rep("E09.5",3), rep("E10.5",3), rep("E11.5",3), rep("E12.5",3), rep("E13.5",3), rep("E14.5",3), rep("E15.5",3), rep("E16.5",3))
     df$protein<- protein
     colnames(df)[1]<-"Expression"
     #Get all of the average expressions for each time point
@@ -216,26 +215,48 @@ server <- function(input, output, session) {
     #print(proteinstouse)
     return(rnastouse)
   })
-  
   get_timepoints_rna<- reactive({
     rnatimepointstouse <- input$rnatimepoint
     #print(timepointstouse)
     return(rnatimepointstouse)
   })
+  get_normalized_timepoint_rna<- reactive({
+    rnanormtimepoint<- input$normalize2
+    #print(normtimepoint)
+    return(rnanormtimepoint)
+  })
   
   #Load in the RNA data frame
-  rnavalues<- read.table("https://github.com/drlaurenwasson/ConlonLab-MMExpression/raw/main/files/RNA_masterdf.txt", sep = "\t")
-  
+  rnavalues<- read.table("https://raw.githubusercontent.com/drlaurenwasson/ConlonLab-MMExpression/main/files/dds_combined_normalized_counts_proteingenes.txt", sep = "\t")
   #Get an RNA data frame for the RNAs of interest
-  getdatarna<- function(gene, timepoint){
-    tp<- c("E10.5", "E11.5", "E12.5", "E13.5", "E14.5", "E15.5", "E16.5")
-    normtp<- tp[tp %in% timepoint]
+  getdatarna<- function(gene, rtimepoint, rnormtp){
+    rtp<- c("E09.5", "E10.5", "E12.5", "E14.5", "E16.5")
     gene<- gene[gene %in% rownames(rnavalues)]
     df3<- as.data.frame(t(rnavalues[rownames(rnavalues)==gene,]))
-    df3$day<- c("E10.5", "E11.5", "E12.5", "E13.5", "E14.5", "E15.5", "E16.5")
+    df3$day<- c(rep("E09.5",3), rep("E10.5",3), rep("E12.5",3), rep("E14.5",3), rep("E16.5",3))
     df3$gene<- gene
     colnames(df3)[1]<-"Expression"
-    return(df3)
+    #Get normalized data
+    ravg<- c(mean(df3$Expression[1:3]), mean(df3$Expression[4:6]), mean(df3$Expression[7:9]), mean(df3$Expression[10:12]), mean(df3$Expression[13:15]))
+    names(ravg)<- rtp
+    #Normalize to the time point we want
+    df3$Normalized<- df3$Expression/(ravg[names(ravg) %in% rnormtp])
+    
+    data_summary2 <- function(data, varname, groupnames){
+      require(plyr)
+      summary_func2 <- function(x, col){
+        c(mean = mean(x[[col]], na.rm=TRUE),
+          sd = sd(x[[col]], na.rm=TRUE))
+      }
+      data_sum2<-ddply(data, groupnames, .fun=summary_func2,
+                       varname)
+      data_sum2 <- rename(data_sum2, c("mean" = varname))
+      return(data_sum2)
+    }
+    df4 <- data_summary2(df3, varname="Normalized", 
+                         groupnames=c("day", "gene"))
+    head(df4)
+    return(df4)
   }
   
   #When "rnaupdate" is pressed
@@ -246,29 +267,35 @@ server <- function(input, output, session) {
     #Update the time points to include
     rnatimepointsupdate<- get_timepoints_rna()
     output$rnatimepointstouse<- shiny::renderText({rnatimepointsupdate})
+    #Update the time point to normalize to
+    rnanormalizeupdate<- get_normalized_timepoint_rna()
+    output$rnanormalizedtimepoint<- shiny::renderText({rnanormalizeupdate})
     
     #Get the data for the genes we want and the time points we want
     #A "for loop" iterates through all x in y. In this example, it will perform the code inside the {} for every gene in the variable 'genes' that I defined above.
     for (rna in {rnaupdate}){
       #Check to see if rna is in the database
       if (rna %in% rownames(rnavalues)){
-        b<- getdatarna(rna,{rnatimepointsupdate})
-        df3=rbind(df3,b)
+        b<- getdatarna(rna,{rnatimepointsupdate},{rnanormalizeupdate})
+        df4=rbind(df4,b)
       }
       
     }
     #Subset only the timepoints you want
-    df3<- df3[df3$day %in% {rnatimepointsupdate},]
+    df4<- df4[df4$day %in% {rnatimepointsupdate},]
     #Plot the scatterplot
     output$rnascatterplot <- renderPlot({
-      p2<- ggplot(df3, aes(x=day, y=Expression, group=gene, color=gene)) + 
+      p2<- ggplot(df4, aes(x=day, y=Normalized, group=gene, color=gene)) + 
         geom_line() +
-        geom_point()
-      p2<- p2+labs(title="RNA Expression", x="Day", y = "Log2FC Compared to E9.5")+
+        geom_point() + 
+        geom_errorbar(aes(ymin=Normalized-sd, ymax=Normalized+sd), width=.2,
+                     position=position_dodge(0.05))
+      p2<- p2+labs(title="RNA Expression", x="Day", y = "Avg. Abundance (Normalized)")+
         theme_classic() 
       plot(p2)
       observeEvent(input$rnaupdate, print(as.numeric(input$rnaupdate)))
     })
+    
     
     output$rnaDownloadPlot = downloadHandler(
       file= paste({rnaupdate}, {rnatimepointsupdate}, ".png", sep=""),
@@ -283,39 +310,57 @@ server <- function(input, output, session) {
   get_metadata_rnaprotein<- reactive({
     rnaprotstouse<- input$rnaproteinoi
     rnaprotstouse<-unlist(strsplit(rnaprotstouse, ","))
-    #print(proteinstouse)
     return(rnaprotstouse)
   })
   #Get the RNA time points
   get_timepoints_rnaproteinr<- reactive({
     rnaproteintimepointstouser <- input$rnaproteintimepointr
-    #print(timepointstouse)
     return(rnaproteintimepointstouser)
   })
   #Get the protein time points
   get_timepoints_rnaproteinp<- reactive({
     rnaproteintimepointstousep <- input$rnaproteintimepointp
-    #print(timepointstouse)
     return(rnaproteintimepointstousep)
   })
-  #Get an RNA data frame for the RNAs of interest
-  getdatarnaprotein<- function(gene, rnatimepoint, proteintimepoint ){
+  #Get the normalized time point
+  get_normalized_timepointrp<- reactive({
+    normtimepoint<- input$normalize3
+    return(normtimepoint)
+  })
+  #Get a data frame for the RNAs of interest
+  getdatarnaprotein<- function(gene, rnatimepoint, proteintimepoint, normtpr){
     #Get the RNA
-    rtp<- c("E10.5", "E11.5", "E12.5", "E13.5", "E14.5", "E15.5", "E16.5")
-    normtpr<- rtp[rtp %in% rnatimepoint]
+    rtp<- c("E09.5", "E10.5", "E12.5", "E14.5", "E16.5")
     gene<- gene[gene %in% rownames(rnavalues)]
-    df4<- as.data.frame(t(rnavalues[rownames(rnavalues)==gene,]))
-    df4$day<- c("E10.5", "E11.5", "E12.5", "E13.5", "E14.5", "E15.5", "E16.5")
-    df4$gene<- gene
-    colnames(df4)[1]<-"Expression"
-    
+    df3<- as.data.frame(t(rnavalues[rownames(rnavalues)==gene,]))
+    df3$day<- c(rep("E09.5",3), rep("E10.5",3), rep("E12.5",3), rep("E14.5",3), rep("E16.5",3))
+    df3$gene<- gene
+    colnames(df3)[1]<-"Expression"
+    #Get normalized data
+    ravg<- c(mean(df3$Expression[1:3]), mean(df3$Expression[4:6]), mean(df3$Expression[7:9]), mean(df3$Expression[10:12]), mean(df3$Expression[13:15]))
+    names(ravg)<- rtp
+    #Normalize to the time point we want
+    df3$Normalized<- df3$Expression/(ravg[names(ravg) %in% normtpr])
+    data_summary2 <- function(data, varname, groupnames){
+      require(plyr)
+      summary_func2 <- function(x, col){
+        c(mean = mean(x[[col]], na.rm=TRUE),
+          sd = sd(x[[col]], na.rm=TRUE))
+      }
+      data_sum2<-ddply(data, groupnames, .fun=summary_func2,
+                       varname)
+      data_sum2 <- rename(data_sum2, c("mean" = varname))
+      return(data_sum2)
+    }
+    df4 <- data_summary2(df3, varname="Normalized", 
+                         groupnames=c("day", "gene"))
     #Get the Protein
-    ptp<- c("E09", "E10", "E11", "E12", "E13", "E14", "E15", "E16")
-    normtpp<- ptp[ptp %in% proteintimepoint]
+    ptp<- c("E09.5", "E10.5", "E11.5", "E12.5", "E13.5", "E14.5", "E15.5", "E16.5")
+    normtpp<- normtpr
     protein=gene
     protein<- protein[protein %in% rownames(rawvalues)]
     df5<- as.data.frame(t(rawvalues[rownames(rawvalues)==protein,]))
-    df5$day<- c(rep("E09",3), rep("E10",3), rep("E11",3), rep("E12",3), rep("E13",3), rep("E14",3), rep("E15",3), rep("E16",3))
+    df5$day<- c(rep("E09.5",3), rep("E10.5",3), rep("E11.5",3), rep("E12.5",3), rep("E13.5",3), rep("E14.5",3), rep("E15.5",3), rep("E16.5",3))
     df5$gene<- protein
     colnames(df5)[1]<-"Expression"
     #Get all of the average expressions for each time point
@@ -337,15 +382,12 @@ server <- function(input, output, session) {
     }
     df6 <- data_summary(df5, varname="Normalized", 
                         groupnames=c("day", "gene"))
-    rownames(df6)<- df6$day
-    df4$sd = 0
-    df6<- df6[,c(3,1,2,4)]
-    names(df6)<- names(df4)
+    df4$data<- "RNA"
+    df6$data<- "Protein"
     df7<- rbind(df4,df6)
-    df7$data=c("RNA", "RNA", "RNA", "RNA", "RNA", "RNA", "RNA", "Protein", "Protein", "Protein", "Protein", "Protein", "Protein", "Protein", "Protein")
     return(df7)
   }
-  
+
   
   #When "rnaproteinupdate" is pressed
   shiny::observeEvent(input$rnaprotupdate,{
@@ -358,13 +400,16 @@ server <- function(input, output, session) {
     #Update the RNA time points to include
     rnaprottimepointsupdater<- get_timepoints_rnaproteinr()
     output$rnaprottimepointstouser<- shiny::renderText({rnaprottimepointsupdater})
+    #Update the normalization time point
+    rnaprotnormalizeupdate<- get_normalized_timepointrp()
+    output$rnaprotnormalizedtimepoint<- shiny::renderText({rnaprotnormalizeupdate})
     
     #Get the data for the genes we want and the time points we want
     #A "for loop" iterates through all x in y. In this example, it will perform the code inside the {} for every gene in the variable 'genes' that I defined above.
     for (rna in {rnaprotupdater}){
       #Check to see if rna is in the database
       if (rna %in% rownames(rnavalues)){
-        b<- getdatarnaprotein(rna,{rnaprottimepointsupdater}, {rnaprottimepointsupdatep})
+        b<- getdatarnaprotein(rna,{rnaprottimepointsupdater}, {rnaprottimepointsupdatep},{rnaprotnormalizeupdate})
         dfrp=rbind(dfrp,b)
       }
       
@@ -372,13 +417,13 @@ server <- function(input, output, session) {
     #Subset only the timepoints you want
     dfrp$group<- paste0(dfrp$gene,dfrp$data)
     dfrp<- dfrp[dfrp$day %in% c({rnaprottimepointsupdater},{rnaprottimepointsupdatep}),]
-    sf <- max(dfrp$Expression)
+    sf <- max(dfrp$Normalized)
     #Plot the scatterplot
     output$rnaprotscatter <- renderPlot({
       p3<- ggplot(dfrp) +
-        geom_line(aes(x=day, y=Expression, group=group, color=gene)) +
-        geom_point(aes(x=day, y=Expression, group=group, color=gene, shape = data),size = 2) + 
-        scale_y_continuous(name = "RNA Fold Change", sec.axis = sec_axis( ~.*sf, name="Protein Expression")) + 
+        geom_line(aes(x=day, y=Normalized, group=group, color=gene)) +
+        geom_point(aes(x=day, y=Normalized, group=group, color=gene, shape = data),size = 2) + 
+        scale_y_continuous(name = "RNA Expression", sec.axis = sec_axis( ~.*sf, name="Protein Expression")) + 
         guides(colour = guide_legend(override.aes = list(size=2)))
       p3<- p3+labs(title="RNA/Protein Expression")+
         theme_classic() 
